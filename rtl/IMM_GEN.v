5dfe3dd8ffb00050b6b6faa01e6c2c29d8dfa80d
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2019-10-07 12:30:49 -0500

    Added licence to IMM_GEN module

--- rtl/IMM_GEN.v
+++ rtl/IMM_GEN.v
@@ -1,3 +1,21 @@
+// Immediate generator module for Core101
+// Copyright (C) 2019 Nicolas Rocha Pacheco
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 module IMM_GEN(
   input   [31:0]  ins_input,
   output  [31:0]  imm_value_output
