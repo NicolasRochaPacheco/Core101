4cefc60d1a33a03ba07d7708d6ca1858ab1f5c88
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-02-26 14:47:23 -0500

    IFU Verilog definition

--- rtl/IFU.v
+++ rtl/IFU.v
@@ -1,4 +1,4 @@
-// Core101 instruction fetch unit module definition
+// Core101 instruction fetch unit (IFU) module definition
 // Copyright (C) 2019 Nicolas Rocha Pacheco
 //
 // This program is free software: you can redistribute it and/or modify
@@ -19,40 +19,37 @@
 //  MODULE DEFINITION
 //-----------------------------------------------
 module IFU (
-
-	// Clock and reset input
+	// Clock and reset signal inputs
 	input ifu_clock_in,
 	input ifu_reset_in,
 
-	// Data inputs
-	input [31:0] pc_upd_addr_in,
-	input [31:0] pc_offset_in,
+	// Program counter data inputs
+  input [31:0] pc_addr_in,
+  input [31:0] pc_offset_in,
 
-	// Control inputs
-	input pc_set_ctrl_in,
-	input [1:0] pc_src_mux_ctrl_in,
-	input ins_mem_valid_ctrl_in,
-	input ir_set_ctrl_in,
+  // Instruction register data inputs
+  input [31:0] ir_data_in,
 
-	// Data outputs
-	output [31:0] pc_stored_val_out,
-	output [31:0] ir_stored_val_out,
+  // Program counter data output
+  output [31:0] pc_addr_out,
 
-	// Control outputs
-	output ins_mem_ready_out,
-	output ins_mem_hit_out
+  // Instruction register data output
+  output [31:0] ir_data_out,
 
+  // Control signals
+  input pc_set_in,
+  input [1:0] pc_mux_sel_in,
+  input ir_set_in
 );
 
 
 //-----------------------------------------------
 //  WIRE DEFINITION
 //-----------------------------------------------
-wire [31:0] pc_offset_adder_wire;
-wire [31:0] pc_inc_adder_wire;
-wire [31:0] pc_src_mux_wire;
-wire [31:0] pc_stored_val_wire;
-wire [31:0] ins_mem_fetched_ins_wire;
+wire [31:0] pc_offset_wire; //
+wire [31:0] pc_inc_wire; //
+wire [31:0] pc_src_wire; //
+wire [31:0] pc_addr_wire; //
 
 
 //-----------------------------------------------
@@ -61,33 +58,33 @@ wire [31:0] ins_mem_fetched_ins_wire;
 ADDER #(.DATA_WIDTH(32)) offset_adder (
 	// Data inputs
 	.a_operand_in(pc_offset_in),
-	.b_operand_in(pc_stored_val_wire),
+	.b_operand_in(pc_addr_wire),
 
 	// Data outputs
-	.add_result_out(pc_offset_adder_wire)
+	.add_result_out(pc_offset_wire)
 );
 
 ADDER #(.DATA_WIDTH(32)) inc_adder (
 	// Data inputs
 	.a_operand_in(32'h00000004),
-	.b_operand_in(pc_stored_val_wire),
+	.b_operand_in(pc_addr_wire),
 
 	// Data outputs
-	.add_result_out(pc_inc_adder_wire)
+	.add_result_out(pc_inc_wire)
 );
 
 MUX_B #(.DATA_WIDTH(32)) pc_src_mux (
 	// Data inputs
-	.a_data_src_in(pc_upd_addr_in),
-	.b_data_src_in(pc_offset_adder_wire),
-	.c_data_src_in(pc_inc_adder_wire),
+	.a_data_src_in(pc_addr_in),
+	.b_data_src_in(pc_offset_wire),
+	.c_data_src_in(pc_inc_wire),
 	.d_data_src_in(32'h00000000),
 
 	// Control inputs
-	.data_sel_in(pc_src_mux_ctrl_in),
+	.data_sel_in(pc_mux_sel_in),
 
 	// Data outputs
-	.data_out(pc_src_mux_wire)
+	.data_out(pc_src_wire)
 );
 
 REG #(.DATA_WIDTH(32)) pc (
@@ -96,11 +93,11 @@ REG #(.DATA_WIDTH(32)) pc (
 	.reset_in(ifu_reset_in),
 
 	// Data inputs
-	.data_in(pc_src_mux_wire),
-	.set_in(pc_set_ctrl_in),
+	.data_in(pc_src_wire),
+	.set_in(pc_set_in),
 
 	// Data outputs
-	.data_out(pc_stored_val_wire)
+	.data_out(pc_addr_wire)
 );
 
 REG #(.DATA_WIDTH(32)) ir (
@@ -109,19 +106,16 @@ REG #(.DATA_WIDTH(32)) ir (
 	.reset_in(ifu_reset_in),
 
 	// Data inputs
-	.data_in(ins_mem_fetched_ins_wire),
+	.data_in(ir_data_in),
 
 	// Control inputs
-	.set_in(ir_set_ctrl_in),
+	.set_in(ir_set_in),
 
 	// Data outputs
-	.data_out(ir_stored_val_out)
+	.data_out(ir_data_out)
 );
 
-INS_CACHE_CONTROLLER #(.DATA_WIDTH(32)) ins_cache_controller (
-	// TBD
-);
-
+assign pc_addr_out = pc_addr_wire;
 
 //-----------------------------------------------
 // OUTPUT LOGIC
