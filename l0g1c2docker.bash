89bdd757aab0eaa159ee239c49d397df182157f5
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-03-06 09:18:10 -0500

    Fixed untar bash

--- l0g1c2docker.bash
+++ l0g1c2docker.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # l0g1c2docker.bash
 
-
+# Compress files and copy them inside Docker container
 tar -czf ../../L0G1C101.tar.gz ../../L0G1C101/
 docker cp ../../L0G1C101.tar.gz core101:/opt/L0G1C101.tar.gz
 rm ../../L0G1C101.tar.gz
@@ -9,9 +9,14 @@ rm ../../L0G1C101.tar.gz
 # Compiles and executes in docker container
 echo "Iniciando el contenedor"
 docker start core101
+echo "Extracting files inside container"
+docker exec core101 bash -c "rm -r /opt/L0G1C101"
+docker exec core101 bash -c "tar -xf /opt/L0G1C101.tar.gz -C /opt/"
 echo "Compilando Core101"
 docker exec core101 bash -c "cd /opt/L0G1C101/Core101; make; cd /"
 echo "Simulando"
 docker exec core101 bash -c "./opt/L0G1C101/Core101/obj_dir/VCore101_top"
+echo "Deleting obj_dir for Core101"
+docker exec core101 bash -c "rm -r /opt/L0G1C101/Core101/obj_dir"
 echo "Deteniendo contenedor"
 docker stop core101
