fb88ec496462f9ff7896b2cefd6cdc28a5591051
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-03-05 19:12:32 -0500

    Bash script to run testbenches

---
+++ l0g1c2docker.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# l0g1c2docker.bash
+
+tar -czf L0G1C101.tar.gz L0G1C101/
+docker cp L0G1C101.tar.gz core101:/opt/L0G1C101.tar.gz
+
+# Compiles and executes in docker container
+echo "Iniciando el contenedor"
+docker start core101
+echo "Compilando Core101"
+docker exec core101 bash -c "source /opt/setL0G1C101.bash"
+echo "Simulando"
+docker exec core101 bash -c "./opt/L0G1C101/Core101/obj_dir/VCore101_top"
+echo "Deteniendo contenedor"
+docker stop core101
