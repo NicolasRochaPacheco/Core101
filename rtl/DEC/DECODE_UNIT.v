53409155385da3d1d47675c8cdb7fcc1efb97d12
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-03-05 19:01:41 -0500

    Decode unit module definition

--- rtl/DEC/DECODE_UNIT.v
+++ rtl/DEC/DECODE_UNIT.v
@@ -19,18 +19,44 @@ module DECODE_UNIT (
 
   input [31:0] dec_ins_in,
 
-  output [4:0] dec_gpr_src_a_out
-  output [4:0] dec_gpr_src_b_out
+  output [2:0] exec_unit_sel_out,
+
+  output [4:0] dec_gpr_src_a_out,
+  output [4:0] dec_gpr_src_b_out,
+  output [4:0] dec_gpr_des_out
 );
 
 // RISC-V OUTPUT LOGIC
 // opcode = ins[6:0]
 // src1 = ins[19:15]
 // src2 = ins[24:20]
+// rd = ins[11:7]
 
+// Parameter definition
+parameter ALU_EXEC_SEL = 3'b001;
+parameter LSU_EXEC_SEL = 3'b010;
+parameter VEC_EXEC_SEL = 3'b100;
+
+// Registers definition
+reg [2:0] exec_sel_reg;
+
+// ====================================
+// COMBINATIONAL LOGIC
+// ====================================
+always@(*) begin
+  if (dec_ins_in[6:2] == 5'b01100)
+    exec_sel_reg = ALU_EXEC_SEL;
+  else
+    exec_sel_reg = LSU_EXEC_SEL;
+end
+
+
+// Execution unit selection output
+assign exec_unit_sel_out = exec_sel_reg;
 
 // General purpose registers output
 assign dec_gpr_src_a_out = dec_ins_in[19:15];
 assign dec_gpr_src_b_out = dec_ins_in[24:20];
+assign dec_gpr_des_out = dec_ins_in[11:7];
 
 endmodule
