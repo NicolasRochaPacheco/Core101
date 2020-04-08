a397b48a2c81e4112b3acb83528469a0e76cd420
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-04-08 14:57:35 -0500

    Core101 test program 1 redefinition

--- src/core101_testbench.cpp
+++ src/core101_testbench.cpp
@@ -4,19 +4,27 @@
 #include "verilated.h"
 #include "VCore101_top.h"
 
-void print_sim_status(int data[12]){
-  std::printf("%03i ", data[0]);  // Clack cycle value
-  std::printf("%i ",   data[1]);  // Clock signal value
-  std::printf("%08X ", data[2]);  //
-  std::printf("%08X ", data[3]);
-  std::printf("%02i ", data[4]);
-  std::printf("%02i ", data[5]);
-  std::printf("%02i ", data[6]);
-  std::printf("%01i ", data[7]);
-  std::printf("%01X ", data[8]);
-  std::printf("%01X ", data[9]);
-  std::printf("%01X ", data[10]);
-  std::printf("%i \n", data[11]);
+// This will be stored on a simulation file
+void print_sim_status(int data[18]){
+  std::printf("CC%03i, ", data[0]);  // Clock cycle value
+  std::printf("%08X, ", data[1]); // PC on IF/ID
+  std::printf("%08X, ", data[2]); // PC on ID/IS
+  std::printf("%08X, ", data[3]); // PC on IS/EX
+  std::printf("%08X, ", data[4]); // PC on EX/WB
+  std::printf("%02i, ", data[5]);
+  std::printf("%02i, ", data[6]);
+  std::printf("%02i, ", data[7]);
+  std::printf("%02i, ", data[8]);
+  std::printf("%05i, ", data[9]);
+  std::printf("%05i, ", data[10]);
+  std::printf("%05i, ", data[11]);
+  std::printf("%05i, ", data[12]);
+  std::printf("%05i, ", data[13]);
+  std::printf("%05i, ", data[14]);
+  std::printf("%05i, ", data[15]);
+  std::printf("%01i, ", data[16]);
+  std::printf("%05i, ", data[17]); // Data to be written on RD
+  std::cout << std::endl;
 }
 
 
@@ -29,8 +37,8 @@ int main(int argc, char **argv){
   VCore101_top* core = new VCore101_top;
 
   // Clock signal parameters
-  const int N_CLOCKS = 20;
-  const int RESOLUTION = 10;
+  const int N_CLOCKS = 16;
+  const int RESOLUTION = 2;
 
   int clock_arr[N_CLOCKS*RESOLUTION];
   int cc_val;
@@ -46,8 +54,8 @@ int main(int argc, char **argv){
     }
   }
 
-  // Prints some info
-  std::cout << "CC_VAL; CC; INS_ADDR; INS_DATA; GPRA; GPRB; GPRD; EXEC_SEL; INT_UOP; VEC_UOP; LSU_UOP; IMM_VAL;" << std::endl;
+  // Prints headers
+  std::cout << "CC, PC_IF, PC_IS" << "\n";
 
   // Main simulation cicle
   for (int k=0; k < N_CLOCKS*RESOLUTION; ++k){
@@ -74,27 +82,34 @@ int main(int argc, char **argv){
 
     core->halt_in = halt;
 
-    int data[12];
-
-    data[0] = cc_val;
-    data[1] = clock_arr[k];
-    data[2] = (int) core->ins_mem_addr_out;
-    data[3] = (int) core->ins_data_out;
-    data[4] = (int) core->gpr_a_out;
-    data[5] = (int) core->gpr_b_out;
-    data[6] = (int) core->gpr_rd_out;
-    data[7] = (int) core->exec_unit_sel_out;
-    data[8] = (int) core->int_uop_out;
-    data[9] = (int) core->vec_uop_out;
-    data[10] = (int) core->lsu_uop_out;
-    data[11] = (int) core->imm_value_out;
-
-    // Prints simulation status
-    print_sim_status(data);
-
     // Generates core outputs
     core->eval();
 
+    // An array to store outputs from the core as the simulation runs.
+    int data[18];
+
+    data[0] = cc_val; // Clock cycle value.
+    data[1] = (int) core->pc_data_if_id_out;  // PC value on IF/ID
+    data[2] = (int) core->pc_data_id_is_out;  // PC value on ID/IS
+    data[3] = (int) core->pc_data_is_ex_out;  // PC value on IS/EX
+    data[4] = (int) core->pc_data_ex_wb_out;  // PC value on EX/WB
+    data[5] = (int) core->rd_addr_if_id_out;
+    data[6] = (int) core->rd_addr_id_is_out;
+    data[7] = (int) core->rd_addr_is_ex_out;
+    data[8] = (int) core->rd_addr_ex_wb_out;
+    data[9] = (int) core->imm_data_out;
+    data[10] = (int) core->id_rs1_data_out;
+    data[11] = (int) core->id_rs2_data_out;
+    data[12] = (int) core->is_a_src_data_out;
+    data[13] = (int) core->is_b_src_data_out;
+    data[14] = (int) core->int_ex_a_src_data_out;
+    data[15] = (int) core->int_ex_b_src_data_out;
+    data[16] = (int) core->int_uop_out;
+    data[17] = (int) core->wb_data_out;
+
+
+    // Prints simulation status on screen
+    print_sim_status(data);
   }
 
   // Exits
