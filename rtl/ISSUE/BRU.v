152b3a0b0197782aa6d918859479c155b38f3df8
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-04-15 14:35:27 -0500

    Renamed ports on BRU

--- rtl/ISSUE/BRU.v
+++ rtl/ISSUE/BRU.v
@@ -17,63 +17,70 @@
 module BRU #(
   DATA_WIDTH = 32
 )(
-  input [DATA_WIDTH-1:0] rs1_data_in,   // R[rs1] data input
-  input [DATA_WIDTH-1:0] rs2_data_in,   // R[rs2] data input
-  input [DATA_WIDTH-1:0] imm_data_in,   // Immediate value.
-  input [DATA_WIDTH-1:0] pc_data_in,    // Program counter address
-  input [2:0] sel_in,                   // SEL in. (enable)
-  input [3:0] uop_in,                   // uOP input
-  input pred_in,                        // Prediction input
-  output flush_out,                     // Flush signal output
-  output feedback_out,
-  output taken_out,
-  output pc_mux_sel_out,                // PC mux selection output
-  output [DATA_WIDTH-1:0] pc_addr_out   // PC new address output
+  input pred_in,                      // Prediction input
+  input enable_in,                    // Enable input
+  input [3:0] uop_in,                 // uOP input
+  input [DATA_WIDTH-1:0] pc_in,       // Program counter address
+  input [DATA_WIDTH-1:0] rs1_in,      // R[rs1] data input
+  input [DATA_WIDTH-1:0] rs2_in,      // R[rs2] data input
+  input [DATA_WIDTH-1:0] imm_in,      // Immediate value.
+  output flush_out,                   // Flush signal output
+  output taken_out,                   // Signal indicating if a branch was taken
+  output enable_out,                  // Enable signal for predictor
+  output mux_out,                     // PC mux selection output
+  output [DATA_WIDTH-1:0] target_out  // PC new address output
 );
+
 
 // WIRE DEFINITION
 wire flush_wire;
+wire taken_wire;
 wire feedback_wire;
 wire pc_mux_sel_wire; // 1'b1 if the branch is taken, 1'b0 otherwise
 wire [31:0] pc_target_wire;  //
-wire taken_wire;
+
 
 // COMBINATIONAL LOGIC
 always @ (*) begin
-  if (sel_in==3'b011) begin         // If a branch is being executed
-    feedback_wire = 1'b1;           // Enables feedback
+  if (enable_in==1'b1) begin        // If a branch is being executed
+    feedback_wire = 1'b1;           // Sets feedback enable
 
     case(uop_in)  // Resolves the branch
-      4'b0000: taken_wire = (rs1_data_in==rs2_data_in) ? 1'b1 : 1'b0;
-      4'b0001: taken_wire = (rs1_data_in!=rs2_data_in) ? 1'b1 : 1'b0;
+      4'b0000: taken_wire = (rs1_in == rs2_in) ? 1'b1:1'b0; // BEQ
+      4'b0001: taken_wire = (rs1_in != rs2_in) ? 1'b1:1'b0;
+      4'b0010: taken_wire = (rs1_in <  rs2_in) ? 1'b1:1'b0;  // BLT
+      4'b0011: taken_wire = (rs1_in >= rs2_in) ? 1'b1:1'b0; //
+      4'b0110: taken_wire = 1'b0; //(rs1_data_in<rs2_data_in) ? 1'b1:1'b0;  // BLTU
+      4'b0111: taken_wire = (rs1_in >  rs2_in) ? 1'b1:1'b0;
       default: taken_wire = 1'b0;
     endcase
 
-    if(pred_in==taken_wire) begin   // Decides if a flush is needed
+    if(pred_in==taken_wire) begin   // If the prediction was correct
       flush_wire = 1'b0;            // Flush not needed
       pc_mux_sel_wire = 1'b0;       // PC update not needed
-    end else begin
+    end else begin                  // If the prediction was not correct
       flush_wire = 1'b1;            // Flush needed
       pc_mux_sel_wire = 1'b1;       // PC update needed
       if(taken_wire==1'b1)          // If the branch is taken
-        pc_target_wire = pc_data_in + imm_data_in;
+        pc_target_wire = pc_in + imm_in;
       else
-        pc_target_wire = pc_data_in + 32'h00000004;
+        pc_target_wire = pc_in + 32'h00000004;
     end
   end else begin
     // Set outputs for instructions that are not branches
     feedback_wire = 1'b0;
     pc_mux_sel_wire = 1'b0;
     flush_wire = 1'b0;
+    taken_wire = 1'b0;
     pc_target_wire = 32'h00000000;
   end
 end
 
 // OUTPUT ASSIGNMENT
 assign flush_out = flush_wire;
-assign taken_out = pc_mux_sel_wire;
-assign feedback_out = feedback_wire;
-assign pc_mux_sel_out = pc_mux_sel_wire;
-assign pc_addr_out = pc_target_wire;
+assign taken_out = taken_wire;
+assign enable_out = feedback_wire;
+assign mux_out = pc_mux_sel_wire;
+assign target_out = pc_target_wire;
 
 endmodule
