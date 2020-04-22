6d6c3b9513f9e0995d37ff05a65f09ed793fa576
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-04-22 08:54:42 -0500

    Changed port names to match standard

--- rtl/IFU/PREDICTOR.v
+++ rtl/IFU/PREDICTOR.v
@@ -16,20 +16,23 @@
 
 
 module PREDICTOR (
-  input clock_in,
-  input reset_in,
-  input taken_branch_in,          // Taken branch feedback
-  input feedback_enable_in,
-  input [9:0] pc_indx_branch_in,  // Update data for branch
-  input [31:0] ins_data_in,       // INS data input
-  input [31:0] pc_addr_in,        // PC address input
-  output taken_pred_out,
-  output [31:0] pred_pc_out
+  input pred_clock_in,            // Predictor clock input
+  input pred_reset_in,            // Predictor reset input
+  input pred_write_enable_in,     // Enable for writing feedback
+  input pred_taken_in,            // Taken branch feedback
+  input [9:0] pred_indx_in,       // Update addr from branch
+  input [31:0] pred_addr_in,       // PC address input
+  input [31:0] pred_ins_in,       // INS data input
+  output pred_taken_out,          // Prediction output
+  output [31:0] pred_pc_out       // Predicted PC output
 );
 
 // Parameter definition
 parameter MEM_SIZE = 1024;
+
+// Opcodes
 parameter BRANCH = 5'b11000;
+parameter JAL = 5'b11011;
 
 // Memory definition
 reg [1:0] branch_buffer [0:MEM_SIZE-1];
@@ -44,32 +47,36 @@ wire [31:0] target_pc_wire;
 
 // Combinational logic
 always @ ( * ) begin
-  if(ins_data_in[6:2] == BRANCH) begin
-    // Predict if the branch should be taken or not
-    status_wire = branch_buffer[pc_addr_in[9:0]];
-    taken_pred_wire = (status_wire==2'b00) || (status_wire==2'b01) ? 1'b0 : 1'b1;
+  // Immediate generation
+  case(pred_ins_in[6:2])
+    BRANCH:   imm_value_wire = {{ 20{pred_ins_in[31]}},
+                                  pred_ins_in[7],
+                                  pred_ins_in[30:25],
+                                  pred_ins_in[11:8], 1'b0};
+    JAL:      imm_value_wire = {{ 21{pred_ins_in[31]}},
+                                  pred_ins_in[30:20]};
+    default:  imm_value_wire = 32'h00000000;
+  endcase
 
-    // Depending on prediction, PC target value is computed
-    if (taken_pred_wire == 1'b1) begin
-      imm_value_wire = {{ 20{ins_data_in[31]}},     // Generates immediate value
-                          ins_data_in[7],
-                          ins_data_in[30:25],
-                          ins_data_in[11:8], 1'b0};
-      target_pc_wire = imm_value_wire + pc_addr_in; // Computes target PC
-    end else begin
-      target_pc_wire = 32'h00000000;
-    end
-  // Executes if instruction is not a branch
-  end else begin
-    taken_pred_wire = 1'b0;
+  // Prediction if taken or not
+  case(pred_ins_in[6:2])
+    BRANCH:   taken_pred_wire = ~branch_buffer[pred_addr_in[9:0]][1];
+    JAL:      taken_pred_wire = 1'b1;
+    default:  taken_pred_wire = 1'b0;
+  endcase
+
+  // Computes target PC address
+  if(taken_pred_wire==1'b1)
+    target_pc_wire = pred_addr_in + imm_value_wire;
+  else
     target_pc_wire = 32'h00000000;
-  end
 end
 
+
 // Sequential logic
-always @ ( posedge clock_in, posedge reset_in ) begin
+always @ ( posedge pred_clock_in, posedge pred_reset_in ) begin
   // Resets memory if signal is asserted on high
-  if(reset_in == 1'b1) begin
+  if(pred_reset_in == 1'b1) begin
     integer j;
     for (j=0; j<MEM_SIZE; j=j+1) begin
       branch_buffer[j] = 2'b00;
@@ -78,13 +85,13 @@ always @ ( posedge clock_in, posedge reset_in ) begin
 
   // Updates branch history according to feedback
   if(feedback_enable_in == 1'b1) begin
-    old_status_wire = branch_buffer[pc_indx_branch_in];
-    branch_buffer[pc_indx_branch_in] = {old_status_wire[0], taken_branch_in};
+    old_status_wire = branch_buffer[pred_indx_in];
+    branch_buffer[pred_indx_in] = {old_status_wire[0], pred_taken_in};
   end
 end
 
-assign taken_pred_out = taken_pred_wire;
 assign pred_pc_out = target_pc_wire;
+assign pred_taken_out = taken_pred_wire;
 
 
 endmodule
