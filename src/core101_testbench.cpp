a95ffcc619279f5dd93ba8c4117b7fe8afaf6858
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-04-29 11:06:20 -0500

    Added outputs to debug file

--- src/core101_testbench.cpp
+++ src/core101_testbench.cpp
@@ -8,23 +8,21 @@
 #include "VCore101_top.h"
 
 // This will be stored on a simulation file
-void print_sim_status(int data[19], FILE* output){ //std::ofstream& output){
-
-  //output << "For CC" << data[0] << std::endl;
-  //output << "    PC on IF/ID: " << data[1] << std::endl;
-
+void print_sim_status(int data[19], FILE* output){
   std::fprintf(output, "For CC%03i: \n", data[0]);
-  std::fprintf(output, "    PC on IF/ID: %08X \n", data[1]);
-  std::fprintf( output,
-                "    RD: %02i; E: %01i; Data: %08i \n",
-                data[2], data[4], data[3]);
-  std::fprintf(output, "    INT uOP: %02i; Enable \n", data[5]);
-  std::fprintf(output, "    LSU uOP: %02i \n", data[6]);
-  std::fprintf(output, "    VEC uOP: %02i \n", data[7]);
-  std::fprintf(output, "    BRU uOP: %02i; Enable: %i \n", data[8], data[11]);
-  std::fprintf(output, "    Branch taken: %i \n", data[12]);
-  std::fprintf(output, "    BRU A: %08i B: %08i \n", data[13], data[14]);
-  std::fprintf(output, "======================================\n");
+  std::fprintf(output, "    ---------- IF/ID ----------\n");
+  std::fprintf(output, "    Prediction: %01d \n", data[1]);
+  std::fprintf(output, "    PC: %08X; IR: %08X \n", data[2], data[3]);
+  std::fprintf(output, "    ---------- ID/IS ----------\n");
+  std::fprintf(output, "    IMM: %i; FWD: %i; \n", data[4], data[5]);
+  std::fprintf(output, "    ---------- IS/EX ----------\n");
+  std::fprintf(output, "    INT uOP: %01i;\n", data[12]);
+
+  // Output for debug stage
+  std::fprintf(output, "    ---------- EX/WB ----------\n");
+  std::fprintf(output, "    WB data: %i; RD: %02i; E: %01i; \n",
+                       data[17], data[18], data[16]);
+  std::fprintf(output, "=======================================\n");
 }
 
 
@@ -37,7 +35,7 @@ int main(int argc, char **argv){
   VCore101_top* core = new VCore101_top;
 
   // Clock signal parameters
-  const int N_CLOCKS = 64;
+  const int N_CLOCKS = 32;
   const int RESOLUTION = 2;
 
   int clock_arr[N_CLOCKS*RESOLUTION];
@@ -56,11 +54,7 @@ int main(int argc, char **argv){
   date[4] = ltm->tm_min;
   date[5] = 1 + ltm->tm_sec;
 
-  // Creates the file
-  //std::ofstream output_file;
-  //output_file.open("output_data.txt");
-  //output_file << date_text << std::endl;
-
+  // Output file
   FILE *output_file;
   output_file = fopen("output_data.txt", "w");
 
@@ -95,34 +89,29 @@ int main(int argc, char **argv){
       core->reset_in = 0;
     }
 
-    // Halt test
-    if (cc_val > 10 && cc_val < 15){
-      halt = 1;
-    } else {
-      halt = 0;
-    }
-
-    core->halt_in = halt;
-
     // Generates core outputs
     core->eval();
 
     // An array to store outputs from the core as the simulation runs.
     int data[19];
 
-    data[0] = cc_val;                         // Clock cycle value.
-    data[1] = (int) core->pc_data_if_id_out;  // PC value on IF/ID
-    data[2] = (int) core->rd_addr_ex_wb_out;  // RD on WB stage
-    data[3] = (int) core->wb_data_out;        // Data to be written on RD
-    data[4] = (int) core->rd_write_enable_out;// Enable signal for RD
-    data[5] = (int) core->int_uop_out;        // INT uOP on EX
-    data[6] = (int) core->lsu_uop_out;        // LSU uOP on EX
-    data[7] = (int) core->vec_uop_out;        // VEC uOP on EX
-    data[8] = (int) core->bru_uop_out;        // BRU uOP on EX
-    data[11] = (int) core->bru_enable_out;    // BRU enable out
-    data[12] = (int) core->branch_taken_out;  // Branch taken out
-    data[13] = (int) core->bru_a_src_out;     // BRU A data
-    data[14] = (int) core->bru_b_src_out;     // BRU B data
+    data[0] = cc_val;                           // Clock cycle value.
+    // IF/ID
+    data[1] = (int) core->if_id_prediction_out; // Prediction on IF/ID
+    data[2] = (int) core->pc_data_if_id_out;    // PC value on IF/ID
+    data[3] = (int) core->ir_data_if_id_out;    // IR value on IF/ID
+    // ID/IS
+    data[4] = (int) core->imm_data_out;         // Immediate value
+    data[5] = (int) core->id_is_fwd_out;        // FWD sel signal
+
+    // IS/EX
+    data[12] = (int) core->int_uop_out;         // INT uOP data
+
+    // EX/WB
+    data[16] = (int) core->rd_write_enable_out; // RD write enable
+    data[17] = (int) core->wb_data_out;         // WB data output
+    data[18] = (int) core->rd_addr_ex_wb_out;   // RD address
+
 
     // Prints simulation status on screen
     if(clock_arr[k] == 1){
