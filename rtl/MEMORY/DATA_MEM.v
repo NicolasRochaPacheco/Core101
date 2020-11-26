05b554442a6f60e2c8ab7c84ec06cbd6ac1e70bd
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-11-26 13:32:28 -0500

    Data memory ports definition

---
+++ rtl/MEMORY/DATA_MEM.v
@@ -0,0 +1,18 @@
+
+
+
+module DATA_MEM #(
+  parameter XLEN = 32
+)(
+  input data_mem_valid_in,
+  input data_mem_write_in,
+  input [XLEN-1:0] data_mem_addr_in,
+  input [XLEN-1:0] data_mem_data_in,
+  output [XLEN-1:0] data_mem_data_out,
+  output data_mem_ready_out
+);
+
+
+
+
+endmodule
