b166e870503400e41bed730a2aebe8bf2ab80317
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-12-16 09:09:56 -0500

    Changed OPCODE input to 7 bits to match python description

--- rtl/modules/IMM_GEN.v
+++ rtl/modules/IMM_GEN.v
@@ -15,65 +15,55 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 module IMM_GEN (
-  input [4:0] imm_gen_opcode_in,
+  input [6:0] imm_gen_opcode_in,
   input [24:0] imm_gen_ins_in,
-  output [31:0] imm_gen_data_out
+  output reg [31:0] imm_gen_data_out
 );
 
 // PARAMETER DEFINITION
-parameter LOAD = 5'b00000;
-parameter OPIMM = 5'b00100;
-parameter AUIPC = 5'b00101;
-parameter STORE = 5'b01000;
-parameter OP = 5'b01100;
-parameter LUI = 5'b01101;
-parameter BRANCH = 5'b11000;
-parameter JALR = 5'b11001;
-parameter JAL = 5'b11011;
-parameter SYSTEM = 5'b11100;
-parameter OPV = 5'b10101;
-
-// ========================================================
-// WIRE DEFINITION
-// ========================================================
-wire [31:0] imm_wire;
+parameter LOAD = 7'b0000011;
+parameter OPIMM = 7'b0010011;
+parameter AUIPC = 7'b0010111;
+parameter STORE = 7'b0100011;
+parameter OP = 7'b0110011;
+parameter LUI = 7'b0110111;
+parameter BRANCH = 7'b1100011;
+parameter JALR = 7'b1100111;
+parameter JAL = 7'b1101111;
+parameter SYSTEM = 7'b1110011;
+parameter OPV = 7'b1010111;
 
 // ========================================================
 // COMBINATIONAL LOGIC
 // ========================================================
 always @ (*) begin
   case(imm_gen_opcode_in)
-    LOAD:   imm_wire = {{21{imm_gen_ins_in[24]}},
+    LOAD:   imm_gen_data_out = {{21{imm_gen_ins_in[24]}},
                         imm_gen_ins_in[23:13]};
-    OPIMM:  imm_wire = {{21{imm_gen_ins_in[24]}},
+    OPIMM:  imm_gen_data_out = {{21{imm_gen_ins_in[24]}},
                         imm_gen_ins_in[23:13]};
-    AUIPC:  imm_wire = {imm_gen_ins_in[24:5],
+    AUIPC:  imm_gen_data_out = {imm_gen_ins_in[24:5],
                         {12{1'b0}}};
-    STORE:  imm_wire = {{21{imm_gen_ins_in[24]}},
-                        imm_gen_ins_in[23:18],
-                        imm_gen_ins_in[4:0]};
-    OP:     imm_wire = 32'h00000000;
-    LUI:    imm_wire = {imm_gen_ins_in[24:5],
+    STORE:  imm_gen_data_out = {{21{imm_gen_ins_in[24]}}, // 21
+                        imm_gen_ins_in[23:18],            // 6
+                        imm_gen_ins_in[4:0]};             // 5
+    OP:     imm_gen_data_out = 32'h00000000;
+    LUI:    imm_gen_data_out = {imm_gen_ins_in[24:5],
                         {12{1'b0}}};
-    BRANCH: imm_wire = {{20{imm_gen_ins_in[24]}},
-                        imm_gen_ins_in[0],
-                        imm_gen_ins_in[23:18],
-                        imm_gen_ins_in[4:1],
+    BRANCH: imm_gen_data_out = {{20{imm_gen_ins_in[24]}}, // 20
+                        imm_gen_ins_in[0],      // 1
+                        imm_gen_ins_in[23:18],  // 6
+                        imm_gen_ins_in[4:1],    // 4
                         1'b0};
-    JALR:   imm_wire = {{21{imm_gen_ins_in[24]}},
+    JALR:   imm_gen_data_out = {{21{imm_gen_ins_in[24]}},
                         imm_gen_ins_in[23:13]};
-    JAL:    imm_wire = {{12{imm_gen_ins_in[24]}},
+    JAL:    imm_gen_data_out = {{12{imm_gen_ins_in[24]}},
                         imm_gen_ins_in[12:5],
                         imm_gen_ins_in[13],
                         imm_gen_ins_in[23:14],
                         1'b0};
-    default: imm_wire = 32'h00000000;
+    default: imm_gen_data_out = 32'h00000000;
   endcase
 end
-
-// ========================================================
-// OUTPUT LOGIC
-// ========================================================
-assign imm_gen_data_out = imm_wire;
 
 endmodule
